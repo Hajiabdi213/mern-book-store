27de75149872d61739607e9a19c5a97b41c7a4b7
korodhsoaqoon <korodhsoaqoon1@gmail.com>
2024-12-09 05:56:29 +0300

    connected to MongoDb

---
+++ backend/config/db.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+  try {
+    const connection = await mongoose
+      .connect("mongodb://localhost:27017")
+      .then(() => console.log("Connected to MongoDb"));
+  } catch (error) {
+    console.log(error.message);
+    process.exit(1); // 1 means - exit   0 means success
+  }
+};
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () =>
-  console.log(`server is running at http://localhost:${PORT}`)
-);
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`server is running at http://localhost:${PORT}`);
+});
 
 // Routes
 
