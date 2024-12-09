b7ef44c2f840477418d4372283ed80e60c044a7e
korodhsoaqoon <korodhsoaqoon1@gmail.com>
2024-12-09 06:14:03 +0300

    created post end-point

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
   try {
     const connection = await mongoose
-      .connect("mongodb://localhost:27017")
+      .connect("mongodb://localhost:27017/book_store_mern")
       .then(() => console.log("Connected to MongoDb"));
   } catch (error) {
     console.log(error.message);
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
+import Book from "./model/books.model.js";
 dotenv.config();
 const app = express();
+app.use(express.json()); // this allows that we can send JSON data to the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   connectDB();
@@ -20,7 +22,28 @@ app.get("/api/books", (req, res) => res.send({ message: "All Books" }));
 app.get("/api/books/:id", (req, res) => res.send({ message: "Single Book" }));
 
 // POST - Save a Book
-app.post("/api/books", (req, res) => res.send({ message: "Save a Book" }));
+app.post("/api/books", async (req, res) => {
+  // let's get the data we want to save from req.body
+  const book = req.body;
+  // let's check if all fields are filled
+  if (!book.title || !book.price || !book.author || !book.language) {
+    return res
+      .status(400)
+      .send({ success: false, message: "Please fill all the fields" });
+  }
+
+  // let's save the book
+  const newBook = new Book(book);
+  try {
+    await newBook.save();
+    res.status(201).send({
+      success: true,
+      data: newBook,
+    });
+  } catch (error) {
+    res.status(500).send({ success: false, message: `Error - ${error}` });
+  }
+});
 
 // PUT - Update a Book
 
