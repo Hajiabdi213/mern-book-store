e468f46ed0eaf5f984beec78844d3424473580b3
korodhsoaqoon <korodhsoaqoon1@gmail.com>
2024-12-09 05:48:33 +0300

    Created Boilter Plate Add basic API endpoint routes without logic

--- backend/server.js
+++ backend/server.js
@@ -6,3 +6,24 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () =>
   console.log(`server is running at http://localhost:${PORT}`)
 );
+
+// Routes
+
+app.get("/", (req, res) => res.send("Hello Students"));
+
+// GET - ALL BOOKS
+app.get("/api/books", (req, res) => res.send({ message: "All Books" }));
+
+// GET - GET Single Book - books/id
+app.get("/api/books/:id", (req, res) => res.send({ message: "Single Book" }));
+
+// POST - Save a Book
+app.post("/api/books", (req, res) => res.send({ message: "Save a Book" }));
+
+// PUT - Update a Book
+
+app.put("/api/books/:id", (req, res) => res.send({ message: "Update a Book" }));
+
+// DELETE - removing a book
+
+app.delete("/api/books/:id", (req, res) => res.send("Deleting a book"));
