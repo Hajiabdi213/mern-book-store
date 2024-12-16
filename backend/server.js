248642b25bc68394a2ac4b6ed99966b0b5705732
korodhsoaqoon <korodhsoaqoon1@gmail.com>
2024-12-16 06:36:57 +0300

    preparing for deployment

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 import bookRouter from "./routes/book.router.js";
 import cors from "cors";
+import path from "path";
 dotenv.config();
 const app = express();
 app.use(cors());
@@ -11,6 +12,13 @@ app.use(express.json()); // this allows that we can send JSON data to the server
 app.use("/api/books", bookRouter);
 
 const PORT = process.env.PORT || 3000;
+const __dirname = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 app.listen(PORT, () => {
   connectDB();
   console.log(`server is running at http://localhost:${PORT}`);
